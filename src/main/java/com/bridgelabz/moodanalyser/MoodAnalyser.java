64aecbd1498ccc35cfb33a9f69484f5f355e27aa
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-07 14:37:03 +0530

    Added: analyzing mood happy or sad

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,9 +1,22 @@
+
 package com.bridgelabz.moodanalyser;
 
+/**
+ * MoodAnalyser is a class of public type.
+ * In this class we are just analyzing weather user mood is sad or happy.
+ *
+ */
 public class MoodAnalyser {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to mood analyser problem :");
+	/**
+	 * analyseMood is a method to check if sad message
+	 * then contain sad if happy then return happy
+	 * @param message
+	 * @return String value
+	 */
+	public String analyseMood(String message) {
+		
+		if(message.contains("Sad"))
+			return "Sad";
+		return "Happy";
 	}
-
 }
--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,15 +1,36 @@
 package com.bridgelabz.moodanalyser;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
-// Junit test
+/**
+ * MoodAnalyserTest is a class of public type
+ * In this class we are testing user moods
+ * If user states , user is in sad mood, Our mood analyzer returns sad
+ * If user states , user is in happy mood, Our mood analyzer returns happy
+ */
 
 public class MoodAnalyserTest {
-	
+	/**
+	 *@method givenSadMessageReturnSad is to test weather user in sad mood
+	 *@Test user is in sad mood
+	 *@return
+	 */
 	@Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void givenSadMessageReturnSad() {
+		MoodAnalyser moodAnalyserTest = new MoodAnalyser();
+		String mood = moodAnalyserTest.analyseMood("Sad");
+		Assert.assertEquals("Sad", mood);
     }
+	/**
+	 * @method givenHappyMessageReturnHappy is to test for users happy mood.
+	 * @Test If user is in happy mood
+	 * @return Happy
+	 */
+
+	@Test
+	public void givenHappyMessageReturnHappy() {
+		MoodAnalyser MoodAnalyserTest = new MoodAnalyser();
+		String mood = MoodAnalyserTest.analyseMood("Happy");
+		Assert.assertEquals("Happy", mood);
+	}
 }
