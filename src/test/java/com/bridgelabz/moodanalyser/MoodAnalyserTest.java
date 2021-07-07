ba0ec92c0fa97bfd3e0a965827bf292458749196
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-07 11:28:44 +0530

    Added: welcome to mood analyser problem

---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.moodanalyser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+// Junit test
+
+public class MoodAnalyserTest {
+	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
